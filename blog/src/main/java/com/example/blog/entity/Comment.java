3718eaaa43f14578918c868176fb33b03e697be1
YiHwanRyu <yihwandev@gmail.com>
2023-07-07 17:59:45 +0900

    [ADD] spring-practice-level3 초기파일 세팅 완료

---
+++ blog/src/main/java/com/example/blog/entity/Comment.java
@@ -0,0 +1,22 @@
+package com.example.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "comment")
+@NoArgsConstructor
+@Entity
+public class Comment extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "content", nullable = false, length = 500)
+    private String content;
+
+}
